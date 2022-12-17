1e491b6762df8cc807ef5217819e7235d4879f6d
evanchooly <jlee@antwerkz.com>
2022-12-16 21:14:46 -0500

    make the mojo

---
+++ src/main/kotlin/com/antwerkz/build/GradleMojo.kt
@@ -0,0 +1,11 @@
+package com.antwerkz.build
+
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugins.annotations.Mojo
+
+@Mojo(name = "gradle-maven")
+class GradleMojo: AbstractMojo() {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
