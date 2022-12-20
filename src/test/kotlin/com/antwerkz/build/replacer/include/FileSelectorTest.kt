f531791166f851c0c40f2837492057549d3afe72
evanchooly <jlee@antwerkz.com>
2022-12-19 22:41:48 -0500

    update the tests

--- src/test/kotlin/com/antwerkz/build/replacer/include/FileSelectorTest.kt
+++ src/test/kotlin/com/antwerkz/build/replacer/include/FileSelectorTest.kt
@@ -2,8 +2,8 @@ package com.antwerkz.build.replacer.include
 
 import java.io.File
 import java.util.Arrays
-import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.hasItem
+import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.testng.Assert.assertTrue
@@ -26,14 +26,14 @@ class FileSelectorTest {
                 mutableListOf("file3")
             )
         assertThat(files.size, `is`(3))
-        assertThat(files, equalTo(mutableListOf("file1", "file2", "include1")))
+        assertThat(files, hasItems("file1", "file2", "include1"))
     }
 
     @Test
     fun shouldSupportNoExcludes() {
         val files =
             FileSelector.listIncludes(BASE_DIR, mutableListOf("include1", "file*"), emptyList())
-        assertThat(files, equalTo(mutableListOf("file1", "file2", "file3", "include1")))
+        assertThat(files, hasItems("file1", "file2", "file3", "include1"))
     }
 
     @Test
@@ -53,7 +53,7 @@ class FileSelectorTest {
         file.writeText(BASE_DIR)
         val files =
             FileSelector.listIncludes(BACK_DIR_SYMBOL, Arrays.asList(TEST_FILE), emptyList())
-        assertThat(files, equalTo(Arrays.asList(TEST_FILE)))
+        assertThat(files, hasItems(TEST_FILE))
     }
 
     @Test
