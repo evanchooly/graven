3a104164fa532f78f3303c29458e53c98576cd0b
evanchooly <jlee@antwerkz.com>
2023-01-08 00:44:27 -0500

    remove old mojo code

--- mojo/src/main/kotlin/com/antwerkz/build/replacer/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This is code originated from Steven Baker's maven-replacer-plugin.  It has been converted to java and "modernized" in various.
- * Any bugs you may encounter are, almost certainly, not Steven's fault.  Many thanks to Steven for writing this mojo originally.  Even
- * if he did abandon it and us.
- *
- * @see <a href="https://code.google.com/archive/p/maven-replacer-plugin/">The Google code repository</a>
- */
-package com.antwerkz.build.replacer;
